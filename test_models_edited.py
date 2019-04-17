7ae45bf8f650f002266fe8b89bccfdaf0e9f741c
Jose Figueroa <josefigueroa168@gmail.com>
2019-04-17 10:58:25 -0400

    Create functions

--- test_models_edited.py
+++ test_models_edited.py
@@ -20,6 +20,7 @@ import matplotlib.pyplot as plt
 from sklearn.preprocessing import LabelEncoder
 from sklearn.utils import resample
 from sklearn.metrics import f1_score
+import sys
 #%%
 
 def tag2vec(data_df, tag_map):
@@ -34,6 +35,43 @@ def tag2vec(data_df, tag_map):
 def accuracy(cm):
     return np.trace(cm)/np.sum(cm)
 
+def encode(filename):
+    df1 = pd.read_csv(filename)
+    df1.loc[:,'POS':'+2POS'].apply(str)
+    df1.loc[:,'POS':'+2POS'] = df1.loc[:,'POS':'+2POS'].apply(LabelEncoder().fit_transform)
+    return df1
+
+def downsample(df_0, df_1):
+    df_majority_downsampled = resample(df_0, 
+                                 replace=False,    # sample without replacement
+                                 n_samples=df_1.size,     # to match minority class
+                                 random_state=123) 
+    return pd.concat([df_majority_downsampled, df_1])
+
+
+def main():
+    #filename = sys.arg[1]
+    filename = "amr-bank-struct-v1.6-training.csv"
+    df1 = encode(filename)
+    df_downsampled = downsample(df1[df1.isfocus == 0], df1[df1.isfocus == 1])
+    X1 = df_downsampled.loc[:,'index':'+2POS'].values
+    y1 = df_downsampled.loc[:,'isfocus'].values
+    X_train, X_test, Y_train, Y_test = train_test_split(
+        X1, y1, test_size=0.25, stratify = y1,random_state=300)
+    
+    # SVM
+    SVM = svm.SVC(C = 1.0, kernel = 'rbf', random_state = 0)
+    SVM.fit(X_train, Y_train)
+    svm_predictions_train = SVM.predict(X_train)
+    svm_predictions_test = SVM.predict(X_test)
+    print (SVM.score(X_test,Y_test))
+    cm_svm = confusion_matrix(y_true=Y_test, y_pred=svm_predictions_test)
+    print (cm_svm)
+    print (accuracy(cm_svm))
+    print ('f1_score: ',f1_score(y_true=Y_test, y_pred=svm_predictions_test, average='macro')  )
+
+if __name__=="__main__":
+    main()
 #%%
 
 data_df = pd.read_csv("data/amr-bank-struct-v1.6-training.csv")
